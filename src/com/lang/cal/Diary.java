b2c24504c96989dbe577838676c45434a7e1886e
aaaahhhh1818 <aaaaahhhhh1818@hanmail.net>
2021-07-15 11:25:59 +0900

    .

--- src/com/lang/cal/Diary.java
+++ src/com/lang/cal/Diary.java
@@ -106,7 +106,7 @@ public class Diary {
   public void diaryList () {
     try {
       System.out.println("\n[일기장 목록]");
-      msg = "select * from  diary " ; //문자열을 명령어 인식해서 실행하도록 Statement
+      msg = "select wdate from diary order by wdate" ; //문자열을 명령어 인식해서 실행하도록 Statement
       RS = ST.executeQuery(msg);
 
       while(RS.next()==true) {
